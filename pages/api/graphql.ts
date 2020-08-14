56fe671a92d510c198136a4c20daa982bd48c003
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2020-08-14 11:45:33 +0800

    added skills

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -2,7 +2,7 @@ import { ApolloServer } from "apollo-server-micro";
 import { schema } from "src/schema";
 
 const server = new ApolloServer({ schema });
-const handler = server.createHandler({ path: "api/graphql" });
+const handler = server.createHandler({ path: "/api/graphql" });
 
 export const config = {
   api: {
--- src/allTypes/Query.ts
+++ src/allTypes/Query.ts
@@ -1,12 +1,26 @@
-import { queryType } from "@nexus/schema";
+import { queryType, idArg } from "@nexus/schema";
 import { data } from "src/data";
-import { Bio } from "./index";
+import { Bio, Position } from "./index";
 
-export const query = queryType({
+export const Query = queryType({
   definition(t) {
     t.field("bio", {
       type: Bio,
       resolve: () => data.bio,
+    });
+
+    t.list.field("positions", {
+      type: Position,
+      resolve: () => data.positions,
+    });
+
+    t.field("position", {
+      type: Position,
+      description: "Find a position by its ID",
+      nullable: true,
+      args: { id: idArg() },
+      resolve: (root, { id }: { id: string }, ctx) =>
+        data.positions.find((position) => position.id === id),
     });
   },
 });
