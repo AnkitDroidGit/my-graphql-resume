a7dd409b6bce21fb2a515abf9f360f6f572d4fa1
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2020-08-17 12:12:49 +0800

    useQuery Hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,31 @@
 import Head from "next/head";
+import { useQuery, gql } from "@apollo/client";
 import styles from "../styles/Home.module.css";
+
+const ResumeQuery = gql`
+  # Write your query or mutation here
+  query ResumeQuery {
+    bio {
+      name
+      github
+      linkedin
+      name
+      tagline
+    }
+
+    positions {
+      id
+      location
+      title
+      startDate
+      endDate
+      company
+      years
+      months
+      achievements
+    }
+  }
+`;
 
 export default function Home() {
   return (
