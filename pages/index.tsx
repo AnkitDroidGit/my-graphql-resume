a07483e41b5732eddd301f5f703de29ddd3afa29
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2020-08-18 17:12:49 +0800

    starting dislaying content

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,6 @@ const ResumeQuery = gql`
       name
       tagline
     }
-
     positions {
       id
       location
@@ -28,13 +27,26 @@ const ResumeQuery = gql`
 `;
 
 export default function Home() {
+  const { data, error, loading } = useQuery(ResumeQuery);
+  if (error) {
+    return <span>Error... oops!</span>;
+  }
+  if (loading) {
+    return (
+      <header className={styles.header}>
+        <h1>Ankit Kumar</h1>
+        <h2>Fullsatck Developer</h2>
+        <h2>loading...</h2>
+      </header>
+    );
+  }
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Ankit Kumar</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Hi
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </>
   );
 }
